c1ef95f922376ee3b070dea086395efd2d909e35
vanduc2514 <vanduc2514@gmail.com>
2020-03-19 14:58:01 +0700

    Refactor 1st time

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class LeapYear {
     public static void main(String[] args) {
         int year;
+        boolean isLeapYear = false;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a year: ");
         year = scanner.nextInt();
         if (year % 4 == 0) {
             if (year % 100 == 0) {
                 if (year % 400 == 0) {
-                    System.out.printf("%d is a leap year", year);
-                } else {
-                    System.out.printf("%d is not a leap year", year);
+                    isLeapYear = true;
                 }
             } else {
-                System.out.printf("%d is a leap year", year);
+                isLeapYear = true;
             }
+        }
+        if (isLeapYear) {
+            System.out.printf("%d is a leap year", year);
         } else {
             System.out.printf("%d is not a leap year", year);
         }
